3be6efaa46a3559dd9487ebdb9f68ca5d9a1bacd
danx <rushn24@gmail.com>
2024-05-19 18:28:03 -0400

    initial setup

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+/** Routes for demonstrating authentication in Express. */
+
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
+
+router.get('/', (req, res, next) => {
+  res.send("APP IS WORKING!!!")
+})
+
+
+
+module.exports = router;
+
